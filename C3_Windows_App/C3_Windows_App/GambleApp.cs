d2d2005914d6f58151f75dc5d6b34d07b746544d
sheepy-v2 <117262261+sheepy-v2@users.noreply.github.com>
2023-12-06 15:17:20 +0100

    menu voor login/registratie

--- C3_Windows_App/C3_Windows_App/GambleApp.cs
+++ C3_Windows_App/C3_Windows_App/GambleApp.cs
@@ -22,8 +22,35 @@ namespace C3_Windows_App
 
             while (userInput.ToLower() != "x")
             {
-                
+                userInput = ShowLoginMenu();
+
             }
+        }
+        private void LoginUserInput(string userInput)
+        {
+            switch (userInput)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                default:
+                    Console.WriteLine("Incorrect choice...");
+                    // Invalid input
+                    break;
+            }
+            Helpers.Pause();
+        }
+        private string ShowLoginMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. login");
+            Console.WriteLine("2. registreer een nieuw account");
+
+            Console.WriteLine("X. Exit");
+
+            string userInput = Helpers.Ask("Make your choice and press <ENTER>.");
+            return userInput;
         }
     }
 }
---
+++ C3_Windows_App/C3_Windows_App/helper.cs
@@ -0,0 +1,37 @@
+﻿namespace C3_Windows_App
+{
+    internal class Helpers
+    {
+        internal static string Ask(string question)
+        {
+            string? userInput = "";
+
+            while ((userInput is null || userInput == ""))
+            {
+                Console.WriteLine(question);
+                userInput = Console.ReadLine();
+            }
+
+            return userInput;
+        }
+
+        internal static int AskForInt(string question)
+        {
+            int retVal;
+            string userInput = "";
+
+            while (!int.TryParse(userInput, out retVal))
+            {
+                userInput = Ask(question);
+            }
+
+            return retVal;
+        }
+
+        internal static void Pause()
+        {
+            Console.WriteLine("Press <ENTER> to continue...");
+            Console.ReadLine();
+        }
+    }
+}
